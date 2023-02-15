2349d8e50f5d4d3c1ac6fc6ecd00010175723728
khangquay1 <nbana31306@gmail.com>
2023-02-15 15:28:22 +0700

    Cai dat xu ly loi - Buoc5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const contactsRouter = require('./app/routers/contact.router');
+const ApiError = require('./app/api-error')
 
 const app = express()
 
@@ -11,6 +12,24 @@ app.get('/',(req, res) => {
     res.json({ message: "Welcome to contact book application."})
 })
 
-app.use('api/contacts', contactsRouter);
+
+app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => { 
+    //Code ở đây sẽ chạy khi không có route được định nghĩa nào khớp
+    // với yêu cầu. Gọi next() để chuyển đến middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and route calls
+app.use((err, req, res, next) => {
+    //Middle ware xử lí lỗi tập trung.
+    //Trong các đoạn code xử lí ở các route, gọi next(error) sẽ chuyển đên 
+    //middleware xử lí lỗi này.
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    })
+})
 
 module.exports = app;
